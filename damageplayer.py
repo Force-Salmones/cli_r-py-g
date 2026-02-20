985ad8e945157cf0422a64c7a872e9556db9a98c
force-Salmones <forcesalmones@gmail.com>
2026-02-20 12:51:54 -0500

    add game end at 0 health

--- damageplayer.py
+++ damageplayer.py
@@ -1,5 +1,8 @@
 from gamestate import GameState
+import sys
 
 def damage_player(state: GameState, amount: int):
     result = state.player.health + amount
     state.player.health = max(0, min(result, state.player.max_health))
+    if state.player.health == 0:
+        sys.exit("You died!")
