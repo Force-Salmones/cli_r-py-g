319803b5fc123f5f439b014b5019b02139d28ee5
force-Salmones <forcesalmones@gmail.com>
2026-02-20 16:20:06 -0500

    remove dev stuff

--- player.py
+++ player.py
@@ -6,7 +6,7 @@ class Player:
     level: int = 1
     health: int = 10
     max_health: int = 10
-    gold: int = 9
+    gold: int = 1
     floor: int = 1
     pos: tuple[int] = (1,1)
     char: str = '@'
